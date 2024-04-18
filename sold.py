80b1496ed15ca4baec4f1d6cac94c29f60907637
Isman Junian <isman.rahman@tokopedia.com>
2024-04-18 19:26:38 +0700

    adjust wording

--- sold.py
+++ sold.py
@@ -5,7 +5,7 @@ def parse_packages_db_data(raw_db_data):
         html += "<td>%s</td>" % db_data[0] #package name
         html += '<td><a href="/packages/add/%s">%s</a></td>' % (db_data[0], db_data[2]) #package id
         html += '<td><input type="number" id="sold_package_%s" placeholder="0" min="1" required></td>' % db_data[0]
-        html += '<td><button onclick="soldPackage(%s);">Sold</button></td>' % (db_data[0])
+        html += '<td><button onclick="soldPackage(%s);">Adjust</button></td>' % (db_data[0])
         html += "</tr>"
     return html
 
@@ -18,6 +18,6 @@ def parse_items_db_data(raw_db_data):
         html += "<td>%s</td>" % db_data[2]
         html += "<td>%s</td>" % db_data[3]
         html += '<td><input type="number" id="sold_item_%s" placeholder="0" min="1" required></td>' % db_data[0]
-        html += '<td><button onclick="soldItem(%s);">Sold</button></td>' % (db_data[0])
+        html += '<td><button onclick="soldItem(%s);">Adjust</button></td>' % (db_data[0])
         html += "</tr>"
     return html
